14e6d48a81a44bc6b8b2ab6ad0821b687b0607fd
Haamer <haamermarten925@gmail.com>
2023-12-01 08:57:31 +0200

    yes

---
+++ KooliProjekt/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication5.Models
+{
+    public class PagedResult<T> : PagedResultBase
+    {
+        public IList<T> Results { get; set; }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
